2728c5c5d9c28cf31d09e13df9f307cb63c9d19b
Wingez <gustav.ledous@gmail.com>
2017-12-13 00:31:52 +0100

    Initial commit

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+
+setup(name='bitbang',
+      version='1.0',
+      description='',
+      url='',
+      author_email='gustav.ledous@gmail.com',
+      license='MIT',
+      packages=['bitbang'],
+      zip_safe=False,
+      include_package_data=True,
+      install_requires=[
+          'RPi.GPIO'
+          ]
+        )
